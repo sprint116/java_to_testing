b4f932af04b8ad14fdd8bfd6228805adbe85d15f
sprint116 <sprint163116@gmail.com>
2022-04-22 21:04:04 +0400

    Correct codeStyles

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, " + "world!");
+    int n = 6;
+    int s = n * n;
+    System.out.println("Площадь квадрата со стороной " + n + " = " + s);
+
+  }
 }
