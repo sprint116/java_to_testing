08a3d313fb459e9ba2617ee51f8bac205ceb71b2
sprint116 <sprint163116@gmail.com>
2022-04-23 19:28:51 +0400

    Задание №2(п.3)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.Scanner;
+
 public class MyFirstProgram {
+  /*
   public static void main(String[] args) {
-    //String massage = "";
+    String massage = "";
     hello("world");
     hello("user");
     hello("Toaster");
@@ -15,5 +18,31 @@ public class MyFirstProgram {
 
   public static void hello(String message) {
     System.out.println("Hello, " + message + "!");
+  }*/
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите координату P1 по оси 'x': ");
+    double p1x = in.nextDouble();
+    System.out.print("Введите координату P1 по оси 'y': ");
+    double p1y = in.nextDouble();
+    System.out.print("Введите координату P2 по оси 'x': ");
+    double p2x = in.nextDouble();
+    System.out.print("Введите координату P2 по оси 'y': ");
+    double p2y = in.nextDouble();
+
+    System.out.println();
+    System.out.printf("Введены координаты P1(%s, %s) и P2(%s, %s)\n", p1x, p1y, p2x, p2y);
+    in.close();
+
+
+    Point p1 = new Point(p1x, p1y);
+    Point p2 = new Point(p2x, p2y);
+
+    System.out.printf("Расстояние между точками P1 и P2 равно: %s\n", distance(p1, p2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
   }
 }
