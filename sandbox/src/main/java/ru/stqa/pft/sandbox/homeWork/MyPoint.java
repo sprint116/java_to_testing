886718eece59fdbccb4e7fd8f85dddc3323bb2d9
sprint116 <sprint163116@gmail.com>
2022-04-24 19:08:33 +0400

    Возврат в код динамических переменных от пользователя

--- sandbox/src/main/java/ru/stqa/pft/sandbox/homeWork/MyPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/homeWork/MyPoint.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox.homeWork;
 
+import java.util.Scanner;
+
 public class MyPoint {
 
   public static void main(String[] args) {
@@ -7,9 +9,22 @@ public class MyPoint {
   }
 
   public static void pointPosition() {
-    Point p1 = new Point(-1.5, 2);
-    Point p2 = new Point(1, -5.5);
-    System.out.println(p1.x + " " + p2);
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите координату P1 по оси 'x': ");
+    double p1x = in.nextDouble();
+    System.out.print("Введите координату P1 по оси 'y': ");
+    double p1y = in.nextDouble();
+    System.out.print("Введите координату P2 по оси 'x': ");
+    double p2x = in.nextDouble();
+    System.out.print("Введите координату P2 по оси 'y': ");
+    double p2y = in.nextDouble();
+
+    System.out.println();
+    System.out.printf("Введены координаты P1(%s, %s) и P2(%s, %s)\n", p1x, p1y, p2x, p2y);
+    in.close();
+
+    Point p1 = new Point(p1x, p1y);
+    Point p2 = new Point(p2x, p2y);
     System.out.printf("Расстояние между точками P1 и P2 равно: %s\n", p1.distance(p2));
   }
 }
