80fee8b6e7b59ec06ace634a65138c9bb527b460
sprint116 <sprint163116@gmail.com>
2022-04-30 21:06:12 +0400

    Чистка лишних действий в тесте "ContactCreationTests"

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -23,39 +23,28 @@ public class ContactCreationTests {
 
   @Test
   public void testUntitledTestCase() throws Exception {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).clear();
+    wd.get("http://localhost/addressbook");
     wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.cssSelector("input[type=\"submit\"]")).click();
     wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
     wd.findElement(By.name("firstname")).sendKeys("TestName");
     wd.findElement(By.name("lastname")).clear();
     wd.findElement(By.name("lastname")).sendKeys("LastName");
     wd.findElement(By.name("nickname")).clear();
     wd.findElement(By.name("nickname")).sendKeys("testing");
-    wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
     wd.findElement(By.name("address")).sendKeys("Testing");
-    wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
     wd.findElement(By.name("mobile")).sendKeys("+79999999999");
-    wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
     wd.findElement(By.name("email")).sendKeys("test@test.test");
-    wd.findElement(By.name("bday")).click();
     new Select(wd.findElement(By.name("bday"))).selectByVisibleText("17");
-    wd.findElement(By.name("bmonth")).click();
     new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("January");
-    wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
     wd.findElement(By.name("byear")).sendKeys("1990");
-    wd.findElement(By.name("theform")).click();
     wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("address2")).clear();
     wd.findElement(By.name("address2")).sendKeys("TestingAddress");
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     wd.findElement(By.linkText("home page")).click();
