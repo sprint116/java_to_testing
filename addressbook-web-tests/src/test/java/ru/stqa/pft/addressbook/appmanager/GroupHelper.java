96676984859e951848f895c3fec165bd07dc8789
sprint116 <sprint163116@gmail.com>
2022-05-20 20:07:29 +0400

    Рефакторинг модификации групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -68,9 +68,9 @@ public class GroupHelper extends HelperBase {
     returnToGroupPage();
   }
 
-  public boolean isThereAGroup() {
+  /*public boolean isThereAGroup() {
     return !isElementPresent(By.name("selected[]"));
-  }
+  }*/
 
   /*public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -32,14 +31,14 @@ public class HelperBase {
     new Select(wd.findElement(locator)).selectByVisibleText(value);
   }
 
-  public boolean isAlertPresent() {
+  /*public boolean isAlertPresent() {
     try {
       wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
     }
-  }
+  }*/
 
   protected boolean isElementPresent(By locator) {
     try {
