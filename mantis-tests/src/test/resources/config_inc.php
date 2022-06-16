19658c62a7c15e30de4b71fbd14994892224f597
sprint116 <sprint163116@gmail.com>
2022-06-16 18:23:39 +0400

    8.5. Передача файлов на удалённую машину по протоколу FTP

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,8 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysql';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_crypto_master_salt     = '6YXP4r0DmfNRdKHrV51iPiOwbLeiNp2hCC76ZvapAu0=';
+$g_signup_use_captcha = OFF;
