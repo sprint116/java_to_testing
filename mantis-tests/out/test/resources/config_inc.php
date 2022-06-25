cbcc23a39b7ae92e63557a95d5ff51ee0ff7d86a
sprint116 <sprint163116@gmail.com>
2022-06-25 19:15:50 +0400

    Лекция 10.1. Сервер непрерывной интеграции

---
+++ mantis-tests/out/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_default_timezone       = 'Europe/Berlin';
+$g_crypto_master_salt     = 'h/jHViI+sKKE1kbj+e0x7a96yXcYTBoSddCY423Zcjg=';
+$g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
